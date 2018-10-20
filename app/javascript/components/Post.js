334298a3b4763f6b797333512b56b0b2a37ef5ff
si0005hp <si0005hp@gmail.com>
2018-10-20 16:28:01 +0800

    Add setClampVisible in Post component

--- app/javascript/components/Post.js
+++ app/javascript/components/Post.js
@@ -7,13 +7,33 @@ class Post extends Component {
     return moment(date).format("YYYY-MM-DD HH:mm:ss")
   }
 
+  setClampVisible() {
+    let postElm = document.getElementById(this.props.post.id)
+    let postContentElm = postElm.getElementsByClassName('post-content')[0]
+
+    let titleElm = postContentElm.getElementsByClassName('title')[0]
+    let bodyElm = postContentElm.getElementsByClassName('body')[0]
+
+    if (titleElm.clientHeight + bodyElm.clientHeight > postContentElm.clientHeight) {
+      let clampElm = postElm.getElementsByClassName('clamp')[0]
+      clampElm.hidden = false
+    }
+  }
+
+  componentDidMount() {
+    this.setClampVisible()
+  }
+
   render() {
     return (
-      <div className="post" >
-        <a className="title" href={"/posts/" + this.props.post.id} target="_blank">
-          <h1>{this.props.post.title}</h1>
-        </a>
-        <p className="body">{this.props.post.body}</p>
+      <div className="post" id={this.props.post.id} >
+        <div className="post-content">
+          <a className="title" href={"/posts/" + this.props.post.id} target="_blank">
+            <h1>{this.props.post.title}</h1>
+          </a>
+          <p className="body">{this.props.post.body}</p>
+        </div>
+        <span className="clamp" hidden>...</span>
         <time className="date">{this.fmtDate(this.props.post.created_at)}</time>
       </div>
     )
