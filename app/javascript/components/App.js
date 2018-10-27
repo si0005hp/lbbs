a101d40d696661ba294592df47148ed62fc560d9
si0005hp <si0005hp@gmail.com>
2018-10-27 23:13:43 +0800

    Comment out App

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import ReactDom from 'react-dom';
-import Header from './Header';
-import PostsContainer from './PostsContainer';
+// import React, { Component } from 'react';
+// import ReactDom from 'react-dom';
+// import Header from './Header';
+// import PostsContainer from './PostsContainer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="main">
-        <Header userId={this.props.userId} />
-        <PostsContainer userId={this.props.userId} />
-      </div>
-    )
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="main">
+//         <Header userId={this.props.userId} />
+//         <PostsContainer userId={this.props.userId} />
+//       </div>
+//     )
+//   }
+// }
 
-// ReactDom.render(<App />, document.getElementById('root'))
-export default App
+// // ReactDom.render(<App />, document.getElementById('root'))
+// export default App
